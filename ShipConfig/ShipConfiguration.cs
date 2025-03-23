71492472e4c9b31b2fe98e6d446fa2fb519ab62c
gglasgow <geraldjglasgow@gmail.com>
2025-03-23 19:19:03 -0400

    fixing mod

---
+++ ShipConfig/ShipConfiguration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using BepInEx.Configuration;
+using ServerSync;
+
+namespace ShipConfig
+{
+    public static class ShipConfiguration
+    {
+        private static ConfigEntry<bool> serverConfigLocked;
+        private static ConfigFile _configFile;
+        private static ConfigSync _configSync;
+        
+        public static void Initialize(ConfigFile configFile, ConfigSync configSync)
+        {
+            _configFile = configFile;
+            _configSync = configSync;
+            serverConfigLocked = CreateConfigEntry("General", "Lock Configuration", true, "[Server Only] The configuration is locked and may not be changed by clients once it has been synced from the server. Only valid for server config, will have no effect on clients.");
+            configSync.AddLockingConfigEntry(serverConfigLocked);
+        }
+        
+        public static ConfigEntry<T> CreateConfigEntry<T>(
+            string group,
+            string name,
+            T defaultValue,
+            string description,
+            bool synchronizedSetting = true)
+        {
+            return CreateConfigEntry(
+                group,
+                name,
+                defaultValue,
+                new ConfigDescription(description),
+                synchronizedSetting
+            );
+        }
+
+        private static ConfigEntry<T> CreateConfigEntry<T>(
+            string group,
+            string name,
+            T defaultValue,
+            ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            if (_configFile == null || _configSync == null)
+            {
+                throw new Exception(
+                    "ValheimShipConfig not initialized. Call Initialize() first in your plugin's Awake().");
+            }
+
+            var extendedDesc = new ConfigDescription(
+                description.Description +
+                (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                description.AcceptableValues,
+                description.Tags);
+
+            ConfigEntry<T> configEntry = _configFile.Bind(group, name, defaultValue, extendedDesc);
+
+            SyncedConfigEntry<T> syncedConfigEntry = _configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+    }
+}
---
+++ ShipConfig/ValheimClasses/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using BepInEx.Configuration;
+using HarmonyLib;
+using Jotunn.Managers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ShipConfig.ValheimClasses
+{
+    [HarmonyPatch(typeof(Player), nameof(Player.Awake))]
+    public class Patch_Player_Awake {
+        public static string SHIP_GROUP = "Ship";
+        
+        static void Postfix() {
+            if (SceneManager.GetActiveScene().name.Equals("main")) {
+                SetAllShipHealth();
+            }
+        }
+        
+        public static void SetAllShipHealth()
+        {
+            ShipConfig.LOG.LogInfo("SHIPCONFIGSHIPCONFIGSHIPCONFIGSHIPCONFIG: Looping through Jötunn PrefabManager cache");
+            foreach (KeyValuePair<string, Object> entry in PrefabManager.Cache.GetPrefabs(typeof(Ship)))
+            {
+                Ship ship = (Ship) entry.Value;
+                ShipConfig.LOG.LogInfo($"SHIPCONFIGSHIPCONFIGSHIPCONFIGSHIPCONFIG: found ship with name {ship.name}");
+                if (ship != null)
+                {
+                    WearNTear wnt = ship.GetComponent<WearNTear>();
+                    if (wnt != null)
+                    {
+                        ConfigEntry<float> shipHealth = ShipConfiguration.CreateConfigEntry(SHIP_GROUP, $"{ship.name}.Health", wnt.m_health, $"Sets health value of {ship.name}.");
+                        wnt.m_health = shipHealth.Value;
+                    }
+                }
+            }
+        }
+    }
+}
